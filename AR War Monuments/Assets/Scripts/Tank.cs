8698b718f41f6157023cc6ac6a3a9e649892be7c
hadiDanial <hadiDanial@users.noreply.github.com>
2023-05-15 22:24:06 +0300

    Created Tank script that initializes tanks with colors and models

---
+++ AR War Monuments/Assets/Scripts/Tank.cs
@@ -0,0 +1,53 @@
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Tank : MonoBehaviour
+{
+    public TankSettings tankSettings;
+    public Transform modelTransformParent;
+    public TMP_Text countryName;
+    public RawImage backgroundColorImage;
+    
+    private void Start()
+    {
+        InitializeTank();
+    }
+
+    [ContextMenu("Initialize Tank")]
+    public void InitializeTankEditMode()
+    {
+        ResetTank(true);
+        SetupTank();
+    }    public void InitializeTank()
+    {
+        ResetTank();
+        SetupTank();
+    }
+
+    private void SetupTank()
+    {
+        countryName.text = tankSettings.countryName;
+        backgroundColorImage.color = tankSettings.countryColor;
+        GameObject tankModel = Instantiate(tankSettings.tankModel, modelTransformParent);
+        tankModel.transform.localPosition= Vector3.zero;
+        tankModel.transform.localRotation = Quaternion.identity;
+    }
+
+    private void ResetTank(bool isInEditMode = false)
+    {
+        countryName.text = string.Empty;
+        backgroundColorImage.color = Color.clear;
+        for (int i = modelTransformParent.childCount - 1; i >= 0; i--)
+        {
+            if(isInEditMode)
+                DestroyImmediate(modelTransformParent.GetChild(i).gameObject);
+            else
+                Destroy(modelTransformParent.GetChild(i).gameObject);
+        }
+    }
+}
